284cecefba0a97c2a3b6f2d718bef7d301ca6d53
Munge-flip <ggonzales2@ssct.edu.ph>
2025-11-16 12:27:02 +0800

    commit

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,14 @@
-    <header>
-        <nav>
-            <div class="weblogo-container">
-                <div class="weblogo-frame">
-                    <img src="img/weblogo.png" alt="Logo" class="logo">
-                </div>
+<header>
+    <nav>
+        <div class="weblogo-container">
+            <div class="weblogo-frame">
+                <img src="{{ asset('img/weblogo.png')}}" alt="Logo" class="logo">
             </div>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="profile.html" class="active">Profile</a></li>
-                <li><a href="#">Track Order</a></li>
-            </ul>
-        </nav>
-    </header>
+        </div>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="profile.html" class="active">Profile</a></li>
+            <li><a href="#">Track Order</a></li>
+        </ul>
+    </nav>
+</header>
