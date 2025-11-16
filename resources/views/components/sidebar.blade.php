5ce38968de28b87d17be3620bde71608bd8543bb
Munge-flip <ggonzales2@ssct.edu.ph>
2025-11-16 10:54:09 +0800

    commit

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,23 +1,23 @@
         <aside class="sidebar">
             <div class="sidebar-menu">
-                <button class="menu-btn active" data-section="account">
+                <a href=" {{ url('/user') }} " @class(['menu-btn', 'active'=>request()->is('user')])>
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                         <circle cx="12" cy="7" r="4"></circle>
                     </svg>
                     My Account
-                </button>
-                <button class="menu-btn" data-section="orders">
+                </a>
+                <a href=" {{ url('/user/order') }} " @class(['menu-btn', 'active'=> request()->is('user/order')])>
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18"></path>
                     </svg>
                     Order History
-                </button>
-                <button class="menu-btn" data-section="chat">
+                </a>
+                <a href="{{ url('/user/chat') }}" @class(['menu-btn', 'active'=> request()->is('user/chat')])>
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
                     </svg>
                     Live Chat
-                </button>
+                </a>
             </div>
         </aside>
