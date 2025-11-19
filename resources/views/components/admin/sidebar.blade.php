b6ac26b5013b2fef485202b1bf00c2b3de939fdf
Munge-flip <ggonzales2@ssct.edu.ph>
2025-11-19 10:54:15 +0800

    commit

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -9,13 +9,13 @@
             </svg>
             Overview
         </a>
-        <a href=" {{ url('/admin/order') }} " @class(['menu-btn', 'active'=>request()->is('orders')])>
+        <a href=" {{ url('/admin/order') }} " @class(['menu-btn', 'active'=>request()->is('admin/order')])>
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18"></path>
             </svg>
             Orders
         </a>
-        <a href=" {{ url('/admin/agent') }} " @class(['menu-btn', 'active'=>request()->is('agents')])>
+        <a href=" {{ url('/admin/agent') }} " @class(['menu-btn', 'active'=>request()->is('admin/agent')])>
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
                 <circle cx="9" cy="7" r="4"></circle>
@@ -24,7 +24,7 @@
             </svg>
             Agents
         </a>
-        <a href=" {{ url('/admin/user') }} " @class(['menu-btn', 'active'=>request()->is('users')])>
+        <a href=" {{ url('/admin/user') }} " @class(['menu-btn', 'active'=>request()->is('admin/user')])>
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                 <circle cx="12" cy="7" r="4"></circle>
