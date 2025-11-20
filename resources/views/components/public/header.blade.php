24dfe999ce40d3cfc83f8d3e0dfb93caa565775d
Munge-flip <ggonzales2@ssct.edu.ph>
2025-11-20 17:21:22 +0800

    commit

--- resources/views/components/public/header.blade.php
+++ resources/views/components/public/header.blade.php
@@ -1,14 +1,14 @@
-        <header>
-            <nav>
-                <div class="weblogo-container">
-                    <div class="weblogo-frame">
-                        <img src="/img/weblogo.png" alt="" class="logo" />
-                    </div>
-                </div>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="user-profile.html">Profile</a></li>
-                    <li><a href="#">Track Order</a></li>
-                </ul>
-            </nav>
-        </header>
+<header>
+    <nav>
+        <div class="weblogo-container">
+            <div class="weblogo-frame">
+                <img src="/img/weblogo.png" alt="" class="logo" />
+            </div>
+        </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="user-profile.html">Profile</a></li>
+            <li><a href="#">Track Order</a></li>
+        </ul>
+    </nav>
+</header>
